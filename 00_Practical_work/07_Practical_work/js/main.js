a5e58d4f8bf0ebcdbe9bbd5375545e7f1465efef
zdkazuha <aartemchelyk@gmail.com>
2025-06-02 20:48:20 +0300

    update

--- 00_Practical_work/07_Practical_work/js/main.js
+++ 00_Practical_work/07_Practical_work/js/main.js
@@ -16,16 +16,22 @@ for (let i = 1; i < arr1.length; i++) {
 
 // № 3
 
-for (let i = 0; i < arr1.length; i++) {
-    if (arr1[i] % 7 == 0) {
-        console.log(true);
-        break;
-    }
-    else {
-        console.log(false);
-        break;
-    }
-}
+// for (let i = 0; i < arr1.length; i++) {
+//     if (arr1[i] % 7 == 0) {
+//         console.log(true);
+//         break;
+//     }
+//     else {
+//         console.log(false);
+//         break;
+//     }
+// }
+
+let arrRes_1 = arr1.filter(a => a % 7 === 0);
+if(arrRes_1.length != 0)
+    console.log(true);
+else 
+    console.log(false);
 
 // № 4
 
@@ -44,15 +50,21 @@ console.log(arr1);
 
 // № 7
 
-for (let i = 0; i < arr1.length; i++) {
-    let number = arr1[i]
-    if (number === arr1[i]) {
-        console.log(true);
-    }
-    else {
-        console.log(false);
-    }
-}
+// for (let i = 0; i < arr1.length; i++) {
+//     let number = arr1[i]
+//     if (number === arr1[i]) {
+//         console.log(true);
+//     }
+//     else {
+//         console.log(false);
+//     }
+// }
+
+let arrRes_2 = arr1.filter((a,b) => a === b);
+if(arrRes_2.length != 0)
+    console.log(true);
+else 
+    console.log(false);
 
 // № 8
 
@@ -62,11 +74,93 @@ console.log(arr2);
 // № 9
 
 let counter = 0;
-for (let i = 0; i < arr1.length; i++) {
-    if (arr1[i] !== 0) {
-        if (arr1[i] % 2 === 0)
-            counter++;
+// for (let i = 0; i < arr1.length; i++) {
+//     if (arr1[i] !== 0) {
+//         if (arr1[i] % 2 === 0)
+//             counter++;
+//     }
+// }
+
+let arrRes_3 = arr1.filter(a => a !== 0 && a % 2 === 0 ? counter++ : counter += 0);
+console.log(counter);
+
+// Task 2
+
+// № 10
+
+function Task1 (text){
+    let counter = 0;
+
+    for (let char of text) {
+      if (char === ' ') 
+        counter++;
     }
+    
+    console.log(counter); 
 }
 
-console.log(counter);
+Task1("Привіт !")
+
+// № 11
+
+function Task2(text){
+    return text[0].toUpperCase() + text.slice(1);
+}
+
+console.log(Task2("привіт !"));
+
+// № 12
+
+function Task3(text){
+    let counter = 0;
+
+    if(text[0] !== ' '){
+        counter++;
+    }
+
+    for(let i = 1; i < text.length;i++){
+        if(text[i - 1] !== ' ' && text[i] === ' '){
+            counter++;
+        }
+    }
+    
+    console.log(counter);
+}
+
+Task3("Привіт як ти і що ти робиш")
+
+// № 13
+
+function Task4(text){
+    let arr = [];
+
+    if(text[0] !== ' '){
+        arr.push(text[0].toUpperCase())
+    }
+
+    for(let i = 1; i < text.length;i++){
+        if(text[i - 1] !== ' ' && text[i] === ' '){
+            arr.push(text[i+1].toUpperCase())
+        }
+    }
+
+    return arr.join('');
+}
+
+console.log(Task4("обєктно оріеннтовне програмування"))
+
+function Task5(text) {
+    for (let i = 0; i < text.length; i++) {
+        if (text[i] !== text[text.length - 1 - i]) {
+            return false;
+        }
+    }
+    return true;
+}
+
+console.log(Task5("aba"))
+
+function Task6(text) {
+    
+}
+
