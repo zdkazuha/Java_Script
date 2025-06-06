7f94cfe7694b0334e1dd49d539173f4497aaa1d5
zdkazuha <aartemchelyk@gmail.com>
2025-06-06 20:51:18 +0300

    09_Practical_work_2

--- 00_Practical_work/09_Practical_work/js/main.js
+++ 00_Practical_work/09_Practical_work/js/main.js
@@ -1,12 +1,46 @@
-let p = document.querySelector(".Task1 p");
+// let p = document.querySelector(".Task1 p");
 
-function updateTime() {
-    let date = new Date().toLocaleTimeString();
-    p.textContent = date;
-}
+// function updateTime() {
+//     let date = new Date().toLocaleTimeString();
+//     p.textContent = date;
+// }
 
-updateTime(); 
+// updateTime(); 
 
-setTimeout(() => {
-    location.reload(); 
-}, 1000);
+// setTimeout(() => {
+//     location.reload(); 
+// }, 1000);
+
+let arr = [];
+let wrappers = document.querySelectorAll('.image-wrapper');
+
+wrappers.forEach(wrapper => {
+    let img = wrapper.querySelector('img');
+    let overlay = wrapper.querySelector('.overlay');
+
+    overlay.addEventListener('click', () => {
+        img.classList.remove('hidden');
+        overlay.style.display = 'none';
+
+        arr.push(img);
+
+        if (arr.length === 2) {
+            if (arr[0].src === arr[1].src) {
+                arr = [];
+            } else {
+                setTimeout(() => {
+                    arr[0].classList.add('hidden');
+                    const overlay = arr[0].parentElement.querySelector('.overlay');
+                    overlay.style.display = 'flex';
+
+
+                    arr[1].classList.add('hidden');
+                    const overlay2 = arr[1].parentElement.querySelector('.overlay');
+                    overlay2.style.display = 'flex';
+
+                    arr = [];
+                }, 100);
+            }
+        }
+    });
+});
